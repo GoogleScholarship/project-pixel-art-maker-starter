4c980b5ca4c42ebe7031eee6d41b2a3c94ec7983
americanman <snowboardracer7@gmail.com>
2018-03-04 14:44:02 -0500

    add jQuery to index.html and changed designs.js file added code for building table dynamically

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
 // Select color input
+let color = $('#colorPicker').val();
 // Select size input
-
+let col = $('#inputWeight').val();
+let row = $('#inputHeight').val();
+let table = $('#pixelCanvas')
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(col, row) {
+  for(let i = 0; col > i; i++){
+    table.append(<tr></tr>);
+    for(let y = 0; row > y; y++){
+      table.append(<td></td>);
+      }    
+    }
 }
