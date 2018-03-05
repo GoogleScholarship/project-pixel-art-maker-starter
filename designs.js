d8daa03da553aca2e01bba90cab01d8096e6d304
americanman <snowboardracer7@gmail.com>
2018-03-04 23:08:48 -0500

    corrected jQuery append order

--- designs.js
+++ designs.js
@@ -3,14 +3,17 @@ let color = $('#colorPicker').val();
 // Select size input
 let col = $('#inputWeight').val();
 let row = $('#inputHeight').val();
-let table = $('#pixelCanvas')
+let table = $('#pixelCanvas');
+const button = $("input[type='submit']");
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(col, row) {
+function makeGrid() {
   for(let i = 0; col > i; i++){
     table.append(<tr></tr>);
     for(let y = 0; row > y; y++){
-      table.append(<td></td>);
+      table.children().last().append(<td></td>);
       }    
     }
 }
+
+button.on('keypress', makeGrid());
