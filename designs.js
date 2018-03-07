9b451b6b73bd55d90c830a9e3def9fe23c776a99
americanman <snowboardracer7@gmail.com>
2018-03-06 22:57:13 -0500

    grid is working

--- designs.js
+++ designs.js
@@ -1,19 +1,27 @@
+$(function() {
 // Select color input
 let color = $('#colorPicker').val();
 // Select size input
-let col = $('#inputWeight').val();
-let row = $('#inputHeight').val();
+
 let table = $('#pixelCanvas');
-const button = $("input[type='submit']");
+let button = $("input[type='submit']");
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  let col = $('#inputHeight').val();
+  let row = $('#inputWeight')[0].value; 
+console.log(col, row);
   for(let i = 0; col > i; i++){
-    table.append(<tr></tr>);
+    table.append('<tr></tr>');
     for(let y = 0; row > y; y++){
-      table.children().last().append(<td></td>);
+      table.children().last().append('<td></td>');
       }    
     }
 }
 
-button.on('keypress', makeGrid());
+button.click(function(event) {
+  event.preventDefault();
+  makeGrid(); 
+});
+}(jQuery));
+
