6f68a09f52bffe2f7b85dbff35ee2042543d0020
americanman <snowboardracer7@gmail.com>
2018-03-11 17:47:42 -0400

    colorPicker works!!!

--- designs.js
+++ designs.js
@@ -10,16 +10,24 @@ let button = $("input[type='submit']");
 function makeGrid() {
  $('table tr').remove(); 
  let col = $('#inputHeight').val();
-  let row = $('#inputWeight')[0].value; 
-console.log(col, row);
+console.log($('#inputWeight').value); 
+console.log($('#inputWeight')[0]); 
+console.log($('#inputWeight')[0].value); 
+
+ let row = $('#inputWeight')[0].value; 
+ console.log(col, row);
   for(let i = 0; col > i; i++){
     table.append('<tr></tr>');
     for(let y = 0; row > y; y++){
       table.children().last().append('<td></td>');
-      }    
-    }
+    }    
+  }
+  table.on("click", "td", 
+  function() {
+    let color = $("input[type='color']#colorPicker").val();
+    $(this).attr("bgcolor", color);
+  });
 }
-
 button.click(function(event) {
   event.preventDefault();
   makeGrid(); 
