fc8bcf6804a1bd72c0ddae44e0e4a25f43d98427
americanman <snowboardracer7@gmail.com>
2018-03-17 21:50:37 -0400

    did refactoring, moved variable color to the global scope and give table removal better name

--- designs.js
+++ designs.js
@@ -3,12 +3,14 @@ $(function() {
   let table = $('#pixelCanvas');
   let button = $("input[type='submit']");
   let clicked = false;
+  let color = $("input[type='color']#colorPicker").val();
 
   // console.log('table value 1', table)
   // When size is submitted by the user, call makeGrid()
   function makeGrid() {
-    // console.log('table value 2', table)
-    table[0].innerHTML = ""
+    $("#pixelCanvas tr").remove();
+    // table[0].innerHTML = ''
+
     let col = $('#inputHeight').val();
     let row = $('#inputWeight')[0].value; 
     for(let i = 0; col > i; i++){
@@ -17,49 +19,41 @@ $(function() {
         table.children().last().append('<td></td>');
       }
     }
-
-    function reached(cell) {
-      let color = $("input[type='color']#colorPicker").val();
-  	  cell.attr("bgcolor", color);
-    }
-
-    table.on("click", "td", function() {
-      // console.log('the frog has leftthe castle. :O ')
-	    // console.log($(this)[0].outerHTML)
-      clicked = false;
-    })
-
-    table.on("mousedown", "td", function(){
-      let color = $("input[type='color']#colorPicker").val();
-      cell=$(this)
-
-      if($(this)[0].outerHTML == '<td bgcolor="'+color+'"></td>'){
-        $(this)[0].outerHTML = '<td bgcolor="#ffffff"></td>'
-        clicked = false
-      } else {
-        reached(cell);
-        clicked = true;
-	    }
-      console.log("mousedown");
-      // set true/false
-    })
-
-    .on("mouseover", "td", function() {
-      // console.log("reached")
-      cell=$(this)
-      // if true
-      // console.log(clicked)
-      if(clicked == true){
-        reached(cell);
-      }
-    })
-
-    $(document).on('mouseup', function() {
-      // console.log("mouse up ");
-      clicked = false;
-      // console.log("reached mouseup number");
-    });
   }
+
+  function changeBackground(cell, currColor) {
+    cell.attr("bgcolor", currColor);
+  }
+
+  table.on("click", "td", function() {
+    clicked = false;
+    console.log("onClick");
+  })
+
+  table.on("mousedown", "td", function(){
+    cell=$(this)
+
+    if($(this)[0].outerHTML == '<td bgcolor="'+color+'"></td>'){
+      changeBackground(cell, "#ffffff")
+      clicked = false
+    } else {
+      changeBackground(cell, color);
+      clicked = true;
+	  }
+    console.log("mousedown");
+  })
+
+  .on("mouseover", "td", function() {
+    cell=$(this)
+    if(clicked == true){
+      changeBackground(cell, color);
+    }
+    console.log("mouseover");
+  })
+
+  $(document).on('mouseup', function() {
+    clicked = false;
+  });
 
   button.click(function(event) {
     makeGrid();
